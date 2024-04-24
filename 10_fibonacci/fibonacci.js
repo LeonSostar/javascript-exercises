bb5c03611e36660069dd2872c90468e2e5eda9c1
Leon Sostar <sostarleon@gmail.com>
2024-04-24 15:35:08 +0200

    Finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x == 0) {
+        return 0;
+    }
+    if (x < 0) {
+        return 'OOPS';
+    }
+    let firstPrev = 1;
+    let secondPrev = 0;
+    for (let i = 2; i <= x; i ++) {
+        let curr = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = curr;
+    }
+    return firstPrev;
 };
 
 // Do not edit below this line
