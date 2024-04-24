8b834ee7090a95e0da18bb11c6f037b12f3c3bf8
Leon Sostar <sostarleon@gmail.com>
2024-04-24 16:02:40 +0200

    Finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldest = 0;
+    let oldestP;
+    arr.forEach(person => {
+        if (getAge(person.yearOfBirth, person.yearOfDeath) > oldest) {
+            oldest = getAge(person.yearOfBirth, person.yearOfDeath);
+            oldestP = person;
+        }
+    });     
+    return oldestP;
 };
+
+const getAge = function(born, dead) {
+    if (!dead) {
+        dead = new Date().getFullYear();
+    }
+    return dead - born;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
