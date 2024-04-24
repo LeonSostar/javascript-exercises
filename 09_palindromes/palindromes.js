e742e8b79fbf9a8a5db85b03e1fa6fb7874278cd
Leon Sostar <sostarleon@gmail.com>
2024-04-24 14:59:01 +0200

    Finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (orgInput) {
+    const regex = /[-.\s*+\/:_,?!^${}()|[\]\\]/g;
+    let newStr = orgInput.replace(regex, '');
+    const compString = newStr.split('').reverse().join('');
+    const str1 = newStr.toUpperCase();
+    const str2 = compString.toUpperCase();
 
+    if (str1 === str2) {
+      return true;
+    }
+    else {
+      return false;
+    }
 };
 
 // Do not edit below this line
